0eb466bf7b92645b2aeedbc061cc591880074f54
aouataf <aouatefd@yahoo.com>
2022-04-23 17:33:03 +0200

    finalized data cleaning

--- wrangling.py
+++ wrangling.py
@@ -1,35 +1,41 @@
 import pandas as pd 
 import numpy as np 
 import re 
-data= pd.read_excel('/home/aouataf/Documents/linkedinJobs/DataAnalystJobs.xlsx', delimiter=",")
 
-# cleaning skills column
-patterns=[
-                 '\n',
-                 '^.*?Expect', 
-                 '^.*?Qualifications', 
-                 '^.*?Required', 
-                 '^.*?expected', 
-                 '^.*?Responsibilities', 
-                 '^.*?Requirements', 
-                 '^.*?QualificationsRequired1', 
-                 '^.*?great', 
-                 '^.*?Looking For', 
-                 '^.*?ll Need', 
-                 ]
+def clean(file):
 
-data['Requirements'] = data['Requirements'].replace( [e for e in patterns] , '', regex=True)
-# remove irrelevent data from industry column 
-data['Industry'] = data['Industry'].str.replace("^(.+?)·",'',regex=True)
+    data= pd.read_excel(delimiter=",")
 
-# move level information from jobtype to level column : [Full-time . associate] ==> [full-time] [associate]
-#level column
-data["Level"]= data['Type'].str.findall("·(.*)$").astype(str)
-#jobtype column 
-data['Type']= data['Type'].str.findall("^(.+?)·").astype(str)
+    # cleaning skills column
+    patterns=[
+                    '\n',
+                    '^.*?Expect', 
+                    '^.*?Qualifications', 
+                    '^.*?Required', 
+                    '^.*?expected', 
+                    '^.*?Responsibilities', 
+                    '^.*?Requirements', 
+                    '^.*?QualificationsRequired1', 
+                    '^.*?great', 
+                    '^.*?Looking For', 
+                    '^.*?ll Need', 
+                    ]
 
+    data['Requirements'] = data['Requirements'].replace( [e for e in patterns] , ' ', regex=True)
+    # remove irrelevent data from industry column 
+    data['Industry'] = data['Industry'].str.replace("^(.+?)·",'',regex=True)
+    # delete salary information from type: $15.50/hr - $17/hr 
+    data['Type']=data['Type'].str.replace("\$.+?·", "",regex=True)
 
+    # move job level information from type to level column : [Full-time . associate] ==> [full-time] [associate]
+    #level column 
+    data[['Type', 'Level']] = data['Type'].str.split('·', 1, expand=True)
 
-#save to excel
-data.to_excel("/home/aouataf/Documents/linkedinJobs/DataAnalystJobs_clean.xlsx")
+    #save to excel
 
+    output="/home/aouataf/Documents/linkedinJobs/DataAnalystJobs_clean.xlsx"
+    data.to_excel(output)
+    return f"data saved to {output}"
+
+dataFile='/home/aouataf/Documents/linkedinJobs/DataAnalystJobs.xlsx'
+clean(dataFile)
